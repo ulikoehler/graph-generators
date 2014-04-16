5d9de3218b2f8e6e5e4683e2e70bd2f1c674d044
Uli Köhler <ulikoehler@online.de>
2014-04-16 18:17:18 +0200

    Added missing random choice algorithms and started to implement Barabasi algorithms

--- Data/Graph/Random/BarabasiAlbert.hs
+++ Data/Graph/Random/BarabasiAlbert.hs
@@ -9,19 +9,19 @@
 module Data.Graph.Random.BarabasiAlbert (
     ) where
 
+import Control.Monad
 import System.Random.MWC
-import System.Random.MWC.CondensedTable
+import Control.Monad.State.Strict
 import Data.Graph.Inductive
-import GHC.Word (Word32)
-import Control.Arrow (second)
+import Control.Applicative
 import Data.IntSet (IntSet)
 import qualified Data.IntSet as IntSet
 import Data.IntMultiSet (IntMultiSet)
-import Data.Vector.Generic (Vector)
-import Data.Vector.Unboxed (fromList)
 import qualified Data.IntMultiSet as IntMultiSet
 
 -- | Select the nth element from a multiset occur list, treating it as virtual large list
+--   This is significantly faster than building up the entire list and selecting the nth
+--   element
 selectNth :: Int -> [(Int, Int)] -> Int
 selectNth _ [] = error "Can't select nth element - n is greater than list size"
 selectNth n ((a,c):xs)
@@ -29,6 +29,8 @@ selectNth n ((a,c):xs)
     | otherwise = selectNth (n-c) xs
 
 -- | Select a single random element from the multiset, with precalculated size
+--   Note that the given size must be the total multiset size, not the number of
+--   distinct elements in said se
 selectRandomElement :: GenIO -> (IntMultiSet, Int) -> IO Int
 selectRandomElement gen (ms, msSize) = do
     let msOccurList = IntMultiSet.toOccurList ms
@@ -40,19 +42,29 @@ selectRandomElement gen (ms, msSize) = do
 --   elements in the multiset. This function accepts a multiset with
 --   precomputed size for performance reasons
 selectNDistinctRandomElements :: GenIO -> Int -> (IntMultiSet, Int) -> IO [Int]
-selectNDistinctRandomElements gen n (ms, msSize) = do
-    let cumList = scanl1 (+) $ IntMultiSet.toOccurList ms
-    undefined
+selectNDistinctRandomElements gen n t@(ms, msSize)
+    | n == msSize = return . map fst . IntMultiSet.toOccurList $ ms
+    | n < msSize = error "Can't select n elements from a set with less than n elements"
+    | otherwise = IntSet.toList <$> selectNDistinctRandomElementsWorker gen n t IntSet.empty
 
 -- | Internal recursive worker for selectNDistinctRandomElements
---selectNDistinctRandomElementsWorker :: GenIO -> Int -> (IntMultiSet, Int) -> IntSet -> IO (Set Int)
---selectNDistinctRandomElementsWorker gen n (ms, msSize) current = do
---    randomElement = selectRandomElement gen 
-
-
+--   Precondition: n > num distinct elems in multiset (not checked)
+--   This implementation is quite naive and selects elements randomly until
+--   a pre
+selectNDistinctRandomElementsWorker :: GenIO -> Int -> (IntMultiSet, Int) -> IntSet -> IO IntSet
+selectNDistinctRandomElementsWorker _ 0 _ current = return current
+selectNDistinctRandomElementsWorker gen n t@(ms, msSize) current = do
+        randomElement <- selectRandomElement gen t
+        let currentWithRE = IntSet.insert randomElement current
+        if randomElement `IntSet.member` current
+            then selectNDistinctRandomElementsWorker gen n t current
+            else selectNDistinctRandomElementsWorker gen (n-1) t currentWithRE
 
 {-
-    Generate a random undirected Barabasi
+    Generate a random quasi-undirected Barabasi graph.
+
+    Only one edge (with nondeterministic direction) is created between a node pair,
+    because adding the other edge direction is easier than removing duplicates.
 
     Modeled after NetworkX 1.8.1 barabasi_albert_graph()
 -}
@@ -61,6 +73,15 @@ barabasiAlbertGraph :: GenIO  -- ^ The random number generator to use
                     -> Int    -- ^ The number of edges to create between a new and existing nodes (m)
                     -> IO UGr -- ^ The resulting graph (IO required for randomness)
 barabasiAlbertGraph gen n m = do
-    let nodes = [1..n] -- Nodes [1..m]: Initial nodes
-    --stateGen =  
-    undefined
+    -- Implementation concept: Iterate over nodes [m..n] in a state monad,
+    --   building up the edge list
+    -- Highly influenced by NetworkX barabasi_albert_graph()
+    let nodes = [1..n] -- Nodes [1..m-1]: Initial nodes
+     -- (Our state: repeated nodes, current targets, edges)
+    let initState = (IntMultiSet.empty, [1..m-1], [])
+    let fn = forM_ [m..n] $ \curNode -> do
+        (repeatedNodes, targets, edges) <- get
+        let msWithSize = (repeatedNodes, IntMultiSet.size repeatedNodes)
+        let newEdges = map (\t -> (curNode, n)) targets
+        newTargets <- liftIO $ selectNDistinctRandomElements gen m msWithSize
+        put (newRepeatedNodes, newTargets, edges ++ newEdges)
